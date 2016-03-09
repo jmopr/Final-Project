9690f9cf4ce35eab9ea6d80cd751304d1bf591b1
Juan Ortiz <jmopr83@gmail.com>
2016-03-09 10:23:35 -0500

    Changes to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ job_data = []
 csv_text1 = File.read('2016-salaries.csv')
 csv1 = CSV.parse(csv_text1, :headers => true)
 csv1.each do |row|
-  unless department_data.include?(row[-1])
-    Department.create(name: row[-1])
-    department_data << row[-1]
+  unless department_data.include?(row["Department"])
+    Department.create(name: row["Department"])
+    department_data << row["Department"]
   end
-  unless job_data.include?(row[1])
-    JobTitle.create(title: row[1])
-    job_data << row[1]
+  unless job_data.include?(row["TITLE"])
+    JobTitle.create(title: row["TITLE"])
+    job_data << row["TITLE"]
   end
 end
 
@@ -44,8 +44,8 @@ csv_text2 = File.read('2016-salaries.csv')
 csv2 = CSV.parse(csv_text2, :headers => true)
 csv2.each do |row|
   name = row[0].split(',')[1].strip.split(' ')[0].capitalize
-  Employee.create(name: row[0], salary: row[4].gsub(/[$,]/,'').strip.to_f,
+  Employee.create(name: row["NAME"], salary: row[4].gsub(/[$,]/,'').strip.to_f,
                   gender: gender.get_gender(name), data_year: 2016,
-                  department_id: Department.find_by(name: row[-1]).id,
-                  job_title_id: JobTitle.find_by(title: row[1]).id)
+                  department_id: Department.find_by(name: row["Department"]).id,
+                  job_title_id: JobTitle.find_by(title: row["TITLE"]).id)
 end
