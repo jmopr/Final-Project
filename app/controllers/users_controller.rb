a674b4c5ea0800f48358e3f2c49c3d1a1f436ef2
Ryan Barrar <Rbarrar17@gmail.com>
2016-03-15 17:49:57 -0400

    did more HTML and turned off turbolinks... they were messing with the html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    # user.api_key = 
    if user.save
      session[:user_id] = user.id
      redirect_to '/'
