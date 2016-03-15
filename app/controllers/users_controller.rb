de8f0fcea464201b158c4a15a88386dfeb411bdf
Ryan Barrar <Rbarrar17@gmail.com>
2016-03-15 19:16:24 -0400

    had to readd turbolinks to have jquery work again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    # user.api_key = 
    if user.save
      session[:user_id] = user.id
      redirect_to '/'
