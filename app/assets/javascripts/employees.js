66137356e3d08be180763eadba2221194b4b40a2
Juan Ortiz <jmopr83@gmail.com>
2016-03-19 00:27:24 -0400

    Fixed data issue and added styling to employees

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -1,17 +1,17 @@
 $(document).on("ready",function(){
-  $( "#department_hover" ).hover(function() {
+  $("#department_hover").hover(function() {
       $(this).append("<a href='/departments'><div class='title_append'><img alt='department_data' id='department_data_graph' src='/assets/department_data' /></div></a>");
     }, function(){
         $(this).find("a:last").remove()
   });
 
-  $( "#job_hover" ).hover(function() {
+  $("#job_hover").hover(function() {
       $(this).append("<a href='/departments/1/year/2016'><div class='title_append'><img alt='job_chart_data' id='job_chart_data_graph' src='/assets/job_chart_data' /></div>");
     }, function(){
         $(this).find("a:last").remove()
   });
 
-  $( "#gender_hover" ).hover(function() {
+  $("#gender_hover").hover(function() {
       $(this).append("<a href='/employees/index'><div class='title_append'><img alt='gender_gap' id='gender_gap_graph' src='/assets/gender_gap_data' /></div>");
     }, function(){
         $(this).find("a:last").remove()
@@ -30,5 +30,40 @@ $(document).on("ready",function(){
     var jobTitleId = $("#job_Title").val();
     var year = $("#year").val();
     window.location = "/departments/" + departmentId + "/job_titles/" + jobTitleId + "/year/" + year;
+  });
+
+  $("#generalbutton").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+       scrollTop: $("#general").offset().top
+    }, 2000);
+  });
+
+  $(".search_button").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+       scrollTop: $("#general").offset().top
+    }, 2000);
+  });
+
+  $("#toptenbutton").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+       scrollTop: $("#topten").offset().top
+    }, 2000);
+  });
+
+  $("#topmenbutton").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+       scrollTop: $("#topmen").offset().top
+    }, 2000);
+  });
+
+  $("#topwomenbutton").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+       scrollTop: $("#topwomen").offset().top
+    }, 2000);
   });
 });
