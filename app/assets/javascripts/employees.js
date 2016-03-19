ccef8521249275fd4947504659dc952bacdcb449
Ryan Barrar <Rbarrar17@gmail.com>
2016-03-19 10:47:28 -0400

    applied employee/index html styling to departments page

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -1,20 +1,20 @@
 $(document).on("ready",function(){
   $("#department_hover").hover(function() {
-      $(this).append("<a href='/departments'><div class='title_append'><img alt='department_data' id='department_data_graph' src='/assets/department_data' /></div></a>");
-    }, function(){
-        $(this).find("a:last").remove()
+    $(this).append("<a href='/departments'><div class='title_append'><img alt='department_data' id='department_data_graph' src='/assets/department_data' /></div></a>");
+  }, function(){
+    $(this).find("a:last").remove()
   });
 
   $("#job_hover").hover(function() {
-      $(this).append("<a href='/departments/1/year/2016'><div class='title_append'><img alt='job_chart_data' id='job_chart_data_graph' src='/assets/job_chart_data' /></div>");
-    }, function(){
-        $(this).find("a:last").remove()
+    $(this).append("<a href='/departments/1/year/2016'><div class='title_append'><img alt='job_chart_data' id='job_chart_data_graph' src='/assets/job_chart_data' /></div>");
+  }, function(){
+    $(this).find("a:last").remove()
   });
 
   $("#gender_hover").hover(function() {
-      $(this).append("<a href='/employees/index'><div class='title_append'><img alt='gender_gap' id='gender_gap_graph' src='/assets/gender_gap_data' /></div>");
-    }, function(){
-        $(this).find("a:last").remove()
+    $(this).append("<a href='/employees/index'><div class='title_append'><img alt='gender_gap' id='gender_gap_graph' src='/assets/gender_gap_data' /></div>");
+  }, function(){
+    $(this).find("a:last").remove()
   });
 
   $("#dept").submit(function(event) {
@@ -35,35 +35,40 @@ $(document).on("ready",function(){
   $("#generalbutton").click(function(event) {
     event.preventDefault();
     $('html, body').animate({
-       scrollTop: $("#general").offset().top
+      scrollTop: $("#general").offset().top
     }, 2000);
   });
 
   $(".search_button").click(function(event) {
-    event.preventDefault();
-    $('html, body').animate({
-       scrollTop: $("#general").offset().top
-    }, 2000);
+    if ($('#dept').addEventListener) {
+      return $('#dept').addEventListener('submit', function(event) {
+        return event.preventDefault();
+      }, true);
+    }
+    // event.preventDefault();
+    // $('html, body').animate({
+    //    scrollTop: $("#general").offset().top
+    // }, 2000);
   });
 
   $("#toptenbutton").click(function(event) {
     event.preventDefault();
     $('html, body').animate({
-       scrollTop: $("#topten").offset().top
+      scrollTop: $("#topten").offset().top
     }, 2000);
   });
 
   $("#topmenbutton").click(function(event) {
     event.preventDefault();
     $('html, body').animate({
-       scrollTop: $("#topmen").offset().top
+      scrollTop: $("#topmen").offset().top
     }, 2000);
   });
 
   $("#topwomenbutton").click(function(event) {
     event.preventDefault();
     $('html, body').animate({
-       scrollTop: $("#topwomen").offset().top
+      scrollTop: $("#topwomen").offset().top
     }, 2000);
   });
 });
